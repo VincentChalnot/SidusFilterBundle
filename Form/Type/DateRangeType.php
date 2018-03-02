52faca9e9bd176f01d882b909bd6f2199fac67a4
Vincent Chalnot <vchalnot@clever-age.com>
2018-03-02 15:40:57 +0100

    Fixing placeholder translation

--- Form/Type/DateRangeType.php
+++ Form/Type/DateRangeType.php
@@ -5,6 +5,7 @@ namespace Sidus\FilterBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Form type to allow picking of a date range
@@ -14,12 +15,25 @@ class DateRangeType extends AbstractType
     public const START_NAME = 'startDate';
     public const END_NAME = 'endDate';
 
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * @todo : FIXME : Inject date/time format from configuration and move bootstrap specific elements to different
      *       bundle !!!
      *
      * @param FormBuilderInterface $builder
      * @param array                $options
+     *
+     * @throws \Symfony\Component\Translation\Exception\InvalidArgumentException
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -33,7 +47,7 @@ class DateRangeType extends AbstractType
                     'attr' => [
                         'class' => 'input-sm form-control',
                         'data-provider' => 'datepicker',
-                        'placeholder' => 'sidus.filter.date_range.start_date',
+                        'placeholder' => $this->translator->trans('sidus.filter.date_range.start_date'),
                     ],
                 ]
             )
@@ -46,7 +60,7 @@ class DateRangeType extends AbstractType
                     'attr' => [
                         'class' => 'input-sm form-control',
                         'data-provider' => 'datepicker',
-                        'placeholder' => 'sidus.filter.date_range.end_date',
+                        'placeholder' => $this->translator->trans('sidus.filter.date_range.end_date'),
                     ],
                 ]
             );
